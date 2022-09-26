4eeb8de74cbf3f68c0974d949525ca38afaee244
Austin Moothart <amooth@gmail.com>
2022-09-26 12:45:54 -0400

    fix build

--- programs/demo-pda/src/lib.rs
+++ programs/demo-pda/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9yQ5rLuamV6voCNZcx7NGUBAT3EXxJvpeGVXZ1MbM9vQ");
 
 #[program]
 pub mod demo_pda {
@@ -19,7 +20,7 @@ pub mod demo_pda {
 } 
 
 #[derive(Accounts)]
-pub struct CreateEscrow<`info> {
+pub struct CreateEscrow<'info> {
     #[account(
         init,
         // Can only have 1 active transaction
@@ -28,15 +29,15 @@ pub struct CreateEscrow<`info> {
         payer = from,
         space = size_of::<EscrowAccount>()
     )]
-    pub escrow: Account<`info, EscrowAccount>(),
+    pub escrow: Account<'info, EscrowAccount>,
 
     #[account(mut)]
-    pub from: Signer<`info`>,
+    pub from: Signer<'info>,
     /// CHECK: safe
     #[account(mut)]
-    pub to: AccountInfo<`info>,
+    pub to: AccountInfo<'info>,
 
-    pub system_program: Program<`info, System>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
